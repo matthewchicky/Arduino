eaf63680474290b1a7225824fedd5129485ea6eb
matthewchicky <matthew.chicky@gmail.com>
2017-02-12 03:22:16 -0500

    Successfully wrote data to EEPROM @ approx 50 hz.

--- chicky_altimeter/chicky_altimeter.ino
+++ chicky_altimeter/chicky_altimeter.ino
@@ -15,34 +15,34 @@
 // Variable Declarations
 byte sta = 0x00;
 byte ctrl = 0x00;
-byte msb=0x00, csb=0x00, lsb=0x00;
+byte msb = 0x00, csb = 0x00, lsb = 0x00;
 byte data1 = 0x00, data2 = 0x00;
-unsigned int address = 0;
+unsigned int address = 65000;
 byte page[128] = {0};
 short int iterator = 0;
 
 void twobyte(byte msb, byte csb, byte lsb, byte *d1, byte *d2)  {
-  long pressure_whole = (long)msb<<16 | (long)csb<<8 | (long)lsb; 
-  pressure_whole >>= 6;                                           
-  pressure_whole -= 88616;    // Apply compression factor         
-  lsb &= B00110000;                                               
-  lsb >>= 4;                                                      
-  long prePackage = pressure_whole<<2 | (long)lsb;
-  
-  *d1 = prePackage>>8;
+  long pressure_whole = (long)msb << 16 | (long)csb << 8 | (long)lsb;
+  pressure_whole >>= 6;
+  pressure_whole -= 88616;    // Apply compression factor
+  lsb &= B00110000;
+  lsb >>= 4;
+  long prePackage = pressure_whole << 2 | (long)lsb;
+
+  *d1 = prePackage >> 8;
   *d2 = (prePackage & B11111111);
 }
 
 float pressure(byte msb, byte csb, byte lsb)
 {
-  long pressure_whole = (long)msb<<16 | (long)csb<<8 | (long)lsb;
+  long pressure_whole = (long)msb << 16 | (long)csb << 8 | (long)lsb;
   pressure_whole >>= 6;
 
   lsb &= B00110000;
   lsb >>= 4;
-  float pressure_decimal = (float)lsb/4.0;
+  float pressure_decimal = (float)lsb / 4.0;
   float pressure = (float)pressure_whole + pressure_decimal;
-  return(pressure);
+  return (pressure);
 }
 
 byte IIC_Read(byte address)
@@ -50,7 +50,7 @@ byte IIC_Read(byte address)
   Wire.beginTransmission(MPL3115A2_ADDRESS);
   Wire.write(address);
   Wire.endTransmission(false);
-  Wire.requestFrom(MPL3115A2_ADDRESS,1);
+  Wire.requestFrom(MPL3115A2_ADDRESS, 1);
   return Wire.read();
 }
 
@@ -62,89 +62,98 @@ void IIC_Write(byte address, byte data)
   Wire.endTransmission(true);
 }
 
-void writeToEEPROM(unsigned int memoryAddress, byte data[], const byte length = 128) {
+void writeToEEPROM(unsigned int memoryAddress, byte data) {
   Wire.beginTransmission(EEPROM_ADDRESS);  // begin transmission with device
   Wire.write(memoryAddress >> 8);
   Wire.write(memoryAddress & 0xFF);
-  for (int i = 0; i<length; ++i)  {
-    Wire.write(data[i]);
-    Serial.println(data[i],BIN);
-  }
+  Wire.write(data);
   Wire.endTransmission();
-  delay(10);
+  delay(5);
 }
 
 byte readFromEEPROM(unsigned int memoryAddress) {
   byte data = 0x00;
-  
+
   Wire.beginTransmission(EEPROM_ADDRESS);  // begin transmission with device
   Wire.write(memoryAddress >> 8);
   Wire.write(memoryAddress & 0xFF);
   Wire.endTransmission();
 
-  Wire.requestFrom(EEPROM_ADDRESS,1);
+  Wire.requestFrom(EEPROM_ADDRESS, 1);
   if (Wire.available()) data = Wire.read();
 
   return data;
+}
+
+void displayEEPROM(unsigned int address)  {
+  while (address == 65535)  {
+    for (unsigned int j = 0; j < 65536; ++j) {
+      Serial.println(readFromEEPROM(j), DEC);
+    }
+  }
 }
 
 void setup() {
   Wire.begin();
   Serial.begin(57600);
   delay(5000);
-  
-  if(IIC_Read(WHO_AM_I) == 196) 
+
+  if (IIC_Read(WHO_AM_I) == 196)
     Serial.println("Pressure sensor online!");
   else
     Serial.println("No response - check connections");
 
-  IIC_Write(CTRL_REG_1,0x00); // 0x00: Standby, 1x Oversample, Barometer Mode
-    if (IIC_Read(CTRL_REG_1)) Serial.println("Failure to set barometer mode, 1x oversample");
-  IIC_Write(CTRL_REG_2,0x0F); // Set the sampling frequency to once every 2^15 sec (0x0F)
-                              // Set the sampling frequency to once every second (0x00)
-    if (IIC_Read(CTRL_REG_2) != 0x0F) Serial.println("Failure to set sampling frequency");
-  IIC_Write(PT_DATA_CFG,0x02); // Generate event flags for new pressure data ONLY
-    if (IIC_Read(PT_DATA_CFG) != 0x02) Serial.println("Failure to set event flags");
+  IIC_Write(CTRL_REG_1, 0x00); // 0x00: Standby, 1x Oversample, Barometer Mode
+  if (IIC_Read(CTRL_REG_1)) Serial.println("Failure to set barometer mode, 1x oversample");
+  IIC_Write(CTRL_REG_2, 0x0F); // Set the sampling frequency to once every 2^15 sec (0x0F)
+  // Set the sampling frequency to once every second (0x00)
+  if (IIC_Read(CTRL_REG_2) != 0x0F) Serial.println("Failure to set sampling frequency");
+  IIC_Write(PT_DATA_CFG, 0x02); // Generate event flags for new pressure data ONLY
+  if (IIC_Read(PT_DATA_CFG) != 0x02) Serial.println("Failure to set event flags");
 }
 
 void loop() {
-  sta = IIC_Read(STATUS);
 
-  if (sta & 0x04){
-    IIC_Write(CTRL_REG_1,0x02);             // 0x02: Standby, OST, 1x Oversample, Barometer
-    msb = IIC_Read(OUT_P_MSB);
-    csb = IIC_Read(OUT_P_CSB);
-    lsb = IIC_Read(OUT_P_LSB);
+  while (address < 65535) {
+    sta = IIC_Read(STATUS);
 
-    twobyte(msb,csb,lsb,&data1,&data2);
-    byte timestep = millis();
+    if (sta == 0x00)  {
+      IIC_Write(CTRL_REG_1, 0x02);            // 0x02: Standby, OST, 1x Oversample, Barometer
+      msb = IIC_Read(OUT_P_MSB);
+      csb = IIC_Read(OUT_P_CSB);
+      lsb = IIC_Read(OUT_P_LSB);
+      ctrl = IIC_Read(CTRL_REG_1);
+    }
+    else if (sta & 0x04) {
+      IIC_Write(CTRL_REG_1, 0x02);            // 0x02: Standby, OST, 1x Oversample, Barometer
+      byte timestamp = millis();
+      msb = IIC_Read(OUT_P_MSB);
+      csb = IIC_Read(OUT_P_CSB);
+      lsb = IIC_Read(OUT_P_LSB);
+      ctrl = IIC_Read(CTRL_REG_1);
 
-    page[iterator] = timestep;
-    Serial.println(timestep,DEC);
-      if (iterator == 127)  {
-        iterator = -1;
-        writeToEEPROM(address,page);
-        while (address == 65408);
-        address += 128;
-      }
-    page[++iterator] = data1;
-    Serial.println(data1,BIN);
-      if (iterator == 127)  {
-        iterator = -1;
-        writeToEEPROM(address,page);
-        while (address == 65408);
-        address += 128;
-      }
-    page[++iterator] = data2;
-    Serial.println(data2,BIN);
-      if (iterator == 127)  {
-        iterator = -1;
-        writeToEEPROM(address,page);
-        while (address == 65408);
-        address += 128;
-      }
-    ++iterator;
+      twobyte(msb, csb, lsb, &data1, &data2);
+
+      writeToEEPROM(address, timestamp);
+      Serial.print(timestamp, DEC);
+      Serial.print("(");
+      Serial.print(address,DEC);
+      Serial.print("),");
+      writeToEEPROM(++address, data1);
+      Serial.print(data1, BIN);
+      Serial.print("(");
+      Serial.print(address,DEC);
+      Serial.print("),");
+      if (address == 65535) displayEEPROM(address);
+      writeToEEPROM(++address, data2);
+      Serial.println(data2, BIN);
+      Serial.print("(");
+      Serial.print(address,DEC);
+      Serial.println(")");
+      if (address == 65535) displayEEPROM(address);
+      ++address;
+    }
   }
-
-  ctrl = IIC_Read(CTRL_REG_1);
+displayEEPROM(address);
 }
+
